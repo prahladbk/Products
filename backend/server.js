f09c52275a041c4ef3e05ea149fdad46750e5f6c
Prahlad bk <prahladbk50@gmail.com>
2025-01-27 20:19:10 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5500
+
+app.use(express.json());
+
+app.use("/api/products",productRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server Started at http://localhost:5500");
+});
